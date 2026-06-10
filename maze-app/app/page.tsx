638389adf2eef0a08c088ee29780a36050fbe6e7
Thomas Newman <tnewman057@gmail.com>
2026-06-10 11:47:17 -0400

    Updates

--- maze-app/app/page.tsx
+++ maze-app/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import MazeGame from './maze'
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col items-center justify-center py-10 px-8 bg-white dark:bg-black">
-        <MazeGame />
+        <Suspense fallback={<div>Loading...</div>}>
+          <MazeGame />
+        </Suspense>
       </main>
     </div>
   );
